f920d49602a779f771ef1dc00092d663d89a4c4a
Herbert Xin <hxin@Herberts-Mac-Studio.local>
2026-01-13 19:47:10 -0800

    Update to v0.2 with sandbox fix

--- Formula/citman.rb
+++ Formula/citman.rb
@@ -1,14 +1,14 @@
 class Citman < Formula
   desc "Citman: A simple citation manager"
   homepage "https://github.com/herbertwxin/Citman"
-  url "https://github.com/herbertwxin/Citman/releases/download/v0.1/Citman-v0.1.tar.gz"
-  sha256 "bc3b40e4e8e8abf0b2fe4d02a14122f987a8c48bd7f6d2e4a20826000e0dd27b"
+  url "https://github.com/herbertwxin/Citman/releases/download/v0.2/Citman-v0.2.tar.gz"
+  sha256 "e920050f494b75541d4554fea18f06a9ed1d48c68b7039756b2122142e5ca9a6"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
 
   def install
-    system "./scripts/build_app.sh"
+    system "./scripts/build_app.sh", "--disable-sandbox"
     prefix.install "Citman.app"
     bin.write_exec_script "#{prefix}/Citman.app/Contents/MacOS/Citman"
   end
